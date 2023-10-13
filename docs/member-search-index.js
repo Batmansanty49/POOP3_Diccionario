aaed650eab5f06d94b339da355720928e99173b8
Batmansanty49 <143793606+Batmansanty49@users.noreply.github.com>
2023-10-12 22:19:32 -0600

    Add files via upload

---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"poop3_diccionario","c":"POOP3_Diccionario","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"poop3_diccionario","c":"POOP3_Diccionario","l":"POOP3_Diccionario()","u":"%3Cinit%3E()"}];updateSearchResults();
---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"poop3_diccionario","l":"POOP3_Diccionario"}];updateSearchResults();
